54107f1a79b759d3d2c0b15efbb295f1178694db
ultimafinal1 <matthewblackman123@gmail.com>
2022-10-11 12:54:28 +0100

    functional route for the app

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ app.listen(4000, () => {
     console.log(`Express departing from 4000`);
 })
 
-/*app.get('/', (req, res) => res.send('Welcome to Budget 2022'));
-
-module.exports = app;*/
-
 // const server = express();
 // server.use(cors());
 // server.use(express.json());
