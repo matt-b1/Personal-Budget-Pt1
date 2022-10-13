c082483490f84680d797f5e9d5b8eb99954d5a89
ultimafinal1 <matthewblackman123@gmail.com>
2022-10-13 12:54:37 +0100

    establish connection from node.js to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connect();
 
 const routes = require('./Routes/budget');
 app.use('/', routes);
