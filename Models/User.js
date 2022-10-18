70f381dc470306d2f16bfedac4fa4c33945c9197
shaeeb <shaeebali@gmail.com>
2022-10-18 13:01:52 +0100

    begin authentication...

--- Models/User.js
+++ Models/User.js
@@ -5,8 +5,16 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {
         type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20
     },
-
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 20
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
