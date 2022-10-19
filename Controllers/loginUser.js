93d2c66b254cb81f101a6d1325b02275895c3431
ultimafinal1 <matthewblackman123@gmail.com>
2022-10-19 12:51:31 +0100

    finish up login

--- Controllers/loginUser.js
+++ Controllers/loginUser.js
@@ -1,16 +1,25 @@
 const User = require('../Models/User');
+const bcrypt = require('bcrypt');
 
 const handleLogin = async (req, res) => {
     const user = req.body;
+    if (!user.username || !user.password) {
+        return res.status(400).json({ 'message': 'Username or password required.'})
+    }
     const foundUser = await User.findOne( { username: user.username }).exec();
     if (foundUser) {
         console.log(`${user.username} IS FOUND`);
-        return res.sendStatus(200);
     } else {
         console.log(`${user.username} IS MISSING`);
-        return res.sendStatus(401);
+        return res.status(401).json({ 'message': 'User not found.'});
     }
-    
+    const match = await bcrypt.compare(user.password, foundUser.password);
+    if (match) {
+        res.json({ 'success': `User ${user.username} has logged in.`});
+    }
+    else {
+        console.log('password fail');
+    }
 }
 
 module.exports = { handleLogin }
