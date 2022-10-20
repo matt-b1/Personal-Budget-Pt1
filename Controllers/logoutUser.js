06a8a6281f01c9c9ff842f5220e0ab65bc39f2f0
ultimafinal1 <matthewblackman123@gmail.com>
2022-10-20 13:15:02 +0100

    implement logout function to database

---
+++ Controllers/logoutUser.js
@@ -0,0 +1,24 @@
+const User = require('../Models/User');
+
+const handleLogout = async (req, res) => {
+    const cookies = req.cookies;
+    console.log(cookies);
+    if (!cookies?.jwt) return res.sendStatus(204); 
+    const refreshToken = cookies.jwt;
+
+    const foundUser = await User.findOne({refreshToken}).exec();
+    if (!foundUser) {
+        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None'}); //add secure
+        return res.sendStatus(204);
+    }
+
+foundUser.refreshToken = '';
+const result = await foundUser.save();
+console.log(result);
+
+res.clearCookie('jwt', { httpOnly: true, sameSite: 'None'}); //add secure
+res.sendStatus(204);
+
+}
+
+module.exports = { handleLogout };
--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const database = require('./config/db.js');
+const cookies = require('cookie-parser');
 
 const app = express();
 
+app.use(cookies());
 app.use(cors());
 app.use(express.json());
 
@@ -12,5 +14,6 @@ database.connect();
 app.use('/', require('./Routes/budget'));
 app.use('/register', require('./Routes/register'));
 app.use('/login', require('./Routes/login'));
+app.use('/logout', require('./Routes/logout'));
 
 module.exports = app;
